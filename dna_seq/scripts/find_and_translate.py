9e8588c9b3981c00789b2c308af5a64418caf6ec
SamAugusto <SamAugusto121@gmail.com>
2026-01-07 23:59:51 -0500

    final hw function changes

--- dna_seq/scripts/find_and_translate.py
+++ dna_seq/scripts/find_and_translate.py
@@ -1,73 +1,77 @@
-import transcribe_dna
-import sys
-def amino_acid_counter(sequence:list)-> int:
-    '''Counts the amino acid lenght of a reading frame'''
-    start = False
-    max_count = 0
-    curr_count = 0
-    
-    stop = {"UAA","UAG","UGA"}
-    for amino in sequence:
-        if amino == "AUG":
-            if start == True:
-                curr_count +=1
-                continue
-            start = True
-            continue
-        if amino in stop:
-            if max_count < curr_count:
-                max_count = curr_count
-                curr_count = 0
-                start = False
-                continue
+from transcribe_dna import seq_transcribe
+def seq_findgene(sequence:str)-> str:
+    transcribed_seq:dict = seq_transcribe(sequence)
 
-            else:
-                curr_count = 0
-                start = False
-                continue
-        if start:
-            curr_count += 1
-            continue
+    forward,reverse= transcribed_seq["mrna"], transcribed_seq["noncode"].replace("T","U")
+    reading_frames_indexes:tuple = (0,1,2)
+    reading_frames:list = []
+
+    for indexes in reading_frames_indexes: # adding all possible reading frames to a list
+        reading_frames.append(forward[indexes:])
+        reading_frames.append(reverse[indexes:])
+
+
+    codon_frames:list = [] 
+    for seq in reading_frames:
+        idx:int = 0
+        sub_codon_frames:list = []
+        while idx <= len (seq):
+            if len(seq[idx:idx+3])==3:
+                sub_codon_frames.append(seq[idx:idx+3])
+            idx +=3
+        codon_frames.append(sub_codon_frames)
+    
+
+    max = 0
+    max_seq = ""
+    stop_codons = {"UAA","UAG","UGA"}
+    for codon_lst in codon_frames:
+        if "AUG" in codon_lst:
+            start = False
+            count = 0
+            codon_count = ""
+            for codon in codon_lst:
+                if start and (codon not in stop_codons):
+                    count += 1
+                    codon_count += codon
+                else:
+                    if not start:
+                        if codon == "AUG":
+                            start = True
+                            count+=1
+                            codon_count +=codon
+                            continue
+                        else:
+                            continue
+                    else:
+                        if max < count:
+                            max = count
+                            max_seq = codon_count+codon
+                            start = False
+                            count = 0
+                            codon_count = ''
+                            continue
+                        else:
+                            continue
         else:
             continue
-    return max_count
-def spliting(sequence:str)-> list:
-    '''Splits the dna seq in'''
-    splitted_seq= []
-
-    index_i = 0
-    index_f = 3
-
-    while index_f <= len(sequence):
-        splitted_seq.append(sequence[index_i:index_f])
-        index_i += 3
-        index_f += 3
-    return splitted_seq
-def reading_frames_assembler(mrna_seq:str,non_coding_dna:str)->set:
-    '''Splits a mrna sequence into 6 reading frames'''
-    reading_frames_indexes= [0,1,2]
-    reading_frames = set()
-    for frames in reading_frames_indexes:
-        reading_frames.add(mrna_seq[frames:])
-        transcribed_non_coding = non_coding_dna.replace("T", "U")
-        reading_frames.add(transcribed_non_coding[frames:])   
-    return reading_frames
+    return seq_transcribe(max_seq)["ptn"]
 
 
 
-def main(test_seq):
-    # Finds the longest reading frame
-    sequence = test_seq
-    # Function imported from other file
-    transcribed_seq_info = transcribe_dna.seq_transcribe(code=sequence)
-    transcribed_mrna_seq,non_coding = transcribed_seq_info["mrna"],transcribed_seq_info["noncode"]
-    all_reading_frames = reading_frames_assembler(transcribed_mrna_seq,non_coding)
-    gene_lenght = [amino_acid_counter(spliting(seqs)) for seqs in all_reading_frames]
-    print(max(gene_lenght),len(gene_lenght))
-    return max(gene_lenght)
- 
-if __name__ == "__main__":
-    main(sys.argv[1])
+   
+
+    
+
+
+
+
+
+
+    
+
+
+
 
 
 
